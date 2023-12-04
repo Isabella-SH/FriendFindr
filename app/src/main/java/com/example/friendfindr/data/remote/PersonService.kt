bb1a3b67362693f9736570a3cf14c1d160746333
Isabella <sorianoisabella28@gmail.com>
2023-12-03 19:19:19 -0500

    implementar la interfaz del servicio

---
+++ app/src/main/java/com/example/friendfindr/data/remote/PersonService.kt
@@ -0,0 +1,15 @@
+package com.example.friendfindr.data.remote
+
+import com.example.friendfindr.data.model.PersonResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface PersonService {
+
+
+    //https://randomuser.me/api/?results=5  ->este es tod el link
+    @GET("?results=5") //colocamos la ultima parte, que es la que llama a todos los resultados
+    fun getAll(): Call<PersonResponse>
+
+
+}
