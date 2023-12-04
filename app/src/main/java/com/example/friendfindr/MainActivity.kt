5f54038de153cf4a90433eda8b2955ab9362ccbe
Isabella <sorianoisabella28@gmail.com>
2023-12-03 20:49:33 -0500

    implementar Home para crear la navegacion y ello llamarlo en el MainActivity.

--- app/src/main/java/com/example/friendfindr/MainActivity.kt
+++ app/src/main/java/com/example/friendfindr/MainActivity.kt
@@ -3,22 +3,18 @@ package com.example.friendfindr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import com.example.friendfindr.ui.personlist.PersonList
-import com.example.friendfindr.ui.personlist.PersonListViewModel
+import com.example.friendfindr.ui.home.Home
 import com.example.friendfindr.ui.theme.FriendFindrTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel:PersonListViewModel by viewModels()
         setContent {
             FriendFindrTheme {  //                                                esta linea no cambiarla!!!!!
 
                 // A surface container using the 'background' color from the theme
-
-                PersonList(viewModel)
+                Home()
             }
         }
     }
---
+++ app/src/main/java/com/example/friendfindr/ui/home/Home.kt
@@ -0,0 +1,96 @@
+package com.example.friendfindr.ui.home
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.friendfindr.ui.personlist.PersonList
+import com.example.friendfindr.ui.personlist.PersonListViewModel
+
+//kotlin->class
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Home() {
+
+    val navController= rememberNavController()
+
+    //crear iteraccion con botones
+    val bottomNavegationItems=listOf(
+        BottomNavigationScreen.PersonList,       //esto yo lo escribo
+    )
+
+    //esta sera la parte de navegacion en la parte inferior
+    Scaffold(
+        bottomBar={
+            AppBottomNavegation(navController,bottomNavegationItems)
+        }
+    ) { paddingValues ->
+
+        Main(navController, modifier= Modifier.padding(paddingValues))
+    }
+}
+
+@Composable
+fun AppBottomNavegation(navController: NavHostController, items: List<BottomNavigationScreen>){
+
+    BottomNavigation {
+
+        val navaBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navaBackStackEntry?.destination
+
+        items.forEach{screen->
+            BottomNavigationItem(
+
+                selected = currentDestination?.hierarchy?.any{
+                    it.route==screen.route
+                }==true,
+                onClick = { navController.navigate(screen.route) },
+                icon = { Icon(screen.icon,null) }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun Main(navController: NavHostController, modifier: Modifier){
+
+    //aqui recien llamo al view model
+    val viewModel:PersonListViewModel= viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = BottomNavigationScreen.PersonList.route, //inicia mostrando la lista de characters
+        modifier
+    ){
+
+        composable(BottomNavigationScreen.PersonList.route){
+            PersonList(viewModel)
+        }
+
+    }
+}
+
+//creamos clase sellada
+sealed class BottomNavigationScreen(val route: String, val icon: ImageVector){
+    object PersonList: BottomNavigationScreen("Personas", Icons.Filled.Home)
+
+}
+
+
