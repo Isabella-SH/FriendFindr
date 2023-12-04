38e28ccb50bdfb4d93e312580061c5576181ab2f
Isabella <sorianoisabella28@gmail.com>
2023-12-03 23:23:34 -0500

    En PersonListViewModel crear funciones de borrar, guardar y obtener todos los person si es favorito. Las obtiene del repository y este del dao

--- app/src/main/java/com/example/friendfindr/ui/personlist/PersonListViewModel.kt
+++ app/src/main/java/com/example/friendfindr/ui/personlist/PersonListViewModel.kt
@@ -3,6 +3,7 @@ import com.example.friendfindr.utils.Result
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.friendfindr.data.local.PersonEntity
 import com.example.friendfindr.data.model.Person
 import com.example.friendfindr.repository.PersonRepository
 
@@ -19,13 +20,34 @@ class PersonListViewModel (
     //devolver la lista
     val people get()=_people
 
+
+                            //devuelve todos las person, metodo del repository que toma del service
     fun getAll(){
 
         personRepository.getAll { result->
             if (result is Result.Success){
-                _people.value=result.data!!.people   //llama a lo que esta en el response
+                _people.value=result.data!!   //llama a lo que esta en el response
             }
         }
     }
 
+
+                                     //metodos del repository que toma del Dao
+
+    //borrar de favoritos
+    fun delete(person: Person){
+        personRepository.delete(person)
+    }
+
+    //guardar como favoritos
+    fun save(person: Person){
+        personRepository.save(person)
+    }
+
+    //devuelva la lista de toda la tabla PersonEntity
+    fun getFavoritePeople(): List<PersonEntity> {
+        return personRepository.getAllFavorites()
+    }
+
 }
+
