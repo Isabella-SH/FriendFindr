3a8f7324bfa246ba15d4ef997bf6387230e48073
Isabella <sorianoisabella28@gmail.com>
2023-12-03 23:07:22 -0500

    En PersonRepository  * su parametro de Dao recibe un context  * el metodo getAll ahora recibe una lista de Person  * Implementar metodos del Dao.  * reconocer que person es un favorito(si se encuentra en la base de datos local)

--- app/src/main/java/com/example/friendfindr/repository/PersonRepository.kt
+++ app/src/main/java/com/example/friendfindr/repository/PersonRepository.kt
@@ -1,5 +1,10 @@
 package com.example.friendfindr.repository
 
+import com.example.friendfindr.App
+import com.example.friendfindr.data.local.AppDataBase
+import com.example.friendfindr.data.local.PersonDao
+import com.example.friendfindr.data.local.PersonEntity
+import com.example.friendfindr.data.model.Person
 import com.example.friendfindr.utils.Result
 import com.example.friendfindr.data.model.PersonResponse
 import com.example.friendfindr.data.remote.ApiClient
@@ -10,12 +15,14 @@ import retrofit2.Response
 
 //kotlin->class
 class PersonRepository(
-    private val personService:PersonService = ApiClient.getPersonService()
+    private val personService:PersonService = ApiClient.getPersonService(),
+    private val personDao: PersonDao= AppDataBase.getInstance(App.instance).personDao(), //al llamarla desde view model, inicializa a characterdao
+
 )
 
 {
                                          //posibles resultados de consumir el api:
-    fun getAll(callback: (Result<PersonResponse>) -> Unit){
+    fun getAll(callback: (Result<List<Person>>) -> Unit){
 
         val getAll=personService.getAll()
 
@@ -30,7 +37,16 @@ class PersonRepository(
 
                 if(response.isSuccessful){
 
-                    callback(Result.Success(data = response.body()!!))
+                    //en esta variable guardo todos los person que se obtuvo del response
+                    val people=response.body()!!.people
+
+                    //de toda esta lista, busco en cada uno
+                    people.forEach{person->
+                        //si este character su atributo de isFavorite es true, creamos un CharacterEntity que almacene este character
+                        //de esta manera se va agregando characters a la base de datos local
+                        person.isFavorite=personDao.getById(person.id.value) !=null
+                    }
+                    callback(Result.Success(data=response.body()!!.people))
                 }
 
             }
@@ -43,4 +59,46 @@ class PersonRepository(
         })
     }
 
+
+                                          //funciones del Dao:
+
+    //elimina un favorito
+    fun delete(person: Person){
+        //en el Dao elimino un PersonEntity con la informacion de un person:Person
+        //colocar datos en orden de como lo recibe PersonEntity
+        personDao.delete(PersonEntity(
+            person.id.value,
+            person.picture.thumbnail,
+            person.name.title,
+            person.name.first,
+            person.gender,
+            person.location.city)
+        )
+        //luego indica que el atributo de isFavorite ahora es false
+        person.isFavorite=false
+    }
+
+    //guarda un favorito
+    fun save(person: Person){
+        //en el Dao guardo un PersonEntity con la informacion de un person:Person
+        personDao.save(PersonEntity(
+            person.id.value,
+            person.picture.thumbnail,
+            person.name.title,
+            person.name.first,
+            person.gender,
+            person.location.city)
+        )
+        //luego indica que el atributo de isFavorite ahora es true
+        person.isFavorite=true
+    }
+
+
+    //devuelve todos los favoritos
+    fun getAllFavorites():List<PersonEntity>{
+        //en el Dao llamo al metodo que devuelve todos los favoritos al realizar el query
+        return personDao.getAll()
+    }
+
 }
+
